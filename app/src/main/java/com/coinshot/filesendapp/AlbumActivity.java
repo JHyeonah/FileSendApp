f0a3dc6f5a0b0f13eecf52b686485a6480cedfed
JHyeonah <gwi02378@gmail.com>
2019-05-14 11:22:56 +0900

    recycler view 스크롤 갱신

--- app/src/main/java/com/coinshot/filesendapp/AlbumActivity.java
+++ app/src/main/java/com/coinshot/filesendapp/AlbumActivity.java
@@ -11,6 +11,7 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.coinshot.filesendapp.adapter.AlbumAdapter;
 import com.coinshot.filesendapp.model.Album;
@@ -19,6 +20,7 @@ import com.coinshot.filesendapp.databinding.ActivityAlbumBinding;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class AlbumActivity extends AppCompatActivity {
     ActivityAlbumBinding bind;
@@ -26,7 +28,11 @@ public class AlbumActivity extends AppCompatActivity {
     private UpdateDBHelper dbHelper;
 
     ArrayList<Album> data;
+    ArrayList<Album> itemList;
     AlbumAdapter adapter;
+    int start = 0;
+    int max = 0;
+    final int PAGE = 4;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,46 +42,105 @@ public class AlbumActivity extends AppCompatActivity {
         db = dbHelper.getWritableDatabase();
 
         data = new ArrayList<>();
+        itemList = new ArrayList<>();
 
         String sql = "select * from picture;";
         Cursor results = db.rawQuery(sql, null);
 
         results.moveToFirst();
         while(!results.isAfterLast()){
-            Log.d("PICTURE", results.getString(3));
-            Log.d("PICTURE", "file name : " + results.getString(1));
             String num = String.valueOf(results.getInt(0));
-            Album album = new Album(num, results.getString(3), results.getString(1), results.getString(4), results.getString(5), results.getString(2));
+            Album album = new Album(results.getInt(0), results.getString(3), results.getString(1), results.getString(4), results.getString(5), results.getString(2));
             data.add(album);
+            Collections.sort(data, myComparator);
 
             results.moveToNext();
         }
         results.close();
+        Collections.sort(data, myComparator);
 
-        Collections.sort(data);
+        max = data.size();
 
-        adapter = new AlbumAdapter(getApplicationContext(), data);
+        for(int i =0; i< max; i++){
+            Log.d("PICTURE", String.valueOf(data.get(i).getNum()));
+        }
 
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
+        if(max < PAGE){
+            for(int i=0; i<max; i++){
+                itemList.add(data.get(i));
+            }
+        }else{
+            for(int i=0; i<PAGE; i++){
+                itemList.add(data.get(i));
+            }
+        }
 
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+
+
+        bind.albumRecycler.addOnScrollListener(recyclerOnScrollListener);
         bind.albumRecycler.setLayoutManager(gridLayoutManager);
+        adapter = new AlbumAdapter(getApplicationContext(),itemList);
         bind.albumRecycler.setAdapter(adapter);
 
     }
-
+/*
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Collections.sort(data);
+        Collections.sort(itemList);
+        adapter.notifyDataSetChanged();
+    }
+*/
     private RecyclerView.OnScrollListener recyclerOnScrollListener = new RecyclerView.OnScrollListener() {
         @Override
         public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
             super.onScrollStateChanged(recyclerView, newState);
+            ArrayList<Album> tempoList = new ArrayList<>();
+            tempoList.clear();
+            tempoList.addAll(itemList);
 
             if(!bind.albumRecycler.canScrollVertically(1)){
-                // 스크롤했을때
+                // 스크롤이 바닥까지 내려왔을 때
+                int start = adapter.getItemCount();
+                Log.d("PICTURE", "start : " + String.valueOf(start));
+                Log.d("PICTURE", "max :  " + String.valueOf(max));
+                int end = start+PAGE-1;
+                Log.d("PICTURE", "end : " + String.valueOf(end));
+
+                if(start >= max-1){
+                    Toast.makeText(getApplicationContext(), "목록의 끝입니다", Toast.LENGTH_SHORT).show();
+                }else if(end >= max){
+                    for(int i = start; i < max; i++){
+                        tempoList.add(data.get(i));
+                        itemList.clear();
+                        itemList.addAll(tempoList);
+                        adapter.notifyDataSetChanged();
+                    }
+                }else{
+                    for(int i = start; i <= end; i++) {
+                        tempoList.add(data.get(i));
+                        itemList.clear();
+                        itemList.addAll(tempoList);
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+
+
             }
         }
 
         @Override
         public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
             super.onScrolled(recyclerView, dx, dy);
+        }
+    };
+
+    private final static Comparator<Album> myComparator = new Comparator<Album>() {
+        @Override
+        public int compare(Album o1, Album o2) {
+            return o2.getNum()-o1.getNum();
         }
     };
 }
--- app/src/main/java/com/coinshot/filesendapp/model/Album.java
+++ app/src/main/java/com/coinshot/filesendapp/model/Album.java
@@ -5,15 +5,15 @@ import android.os.Parcelable;
 
 import java.io.Serializable;
 
-public class Album implements Comparable<Album>, Serializable {
-    String num;
+public class Album implements  Serializable {
+    int num;
     String uri;
     String fileName;
     String title;
     String content;
     String time;
 
-    public Album(String num, String uri, String  fileName, String title, String content, String time){
+    public Album(int num, String uri, String  fileName, String title, String content, String time){
         this.num = num;
         this.uri = uri;
         this.fileName = fileName;
@@ -23,39 +23,11 @@ public class Album implements Comparable<Album>, Serializable {
     }
 /*
     @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(this.num);
-        dest.writeString(this.uri);
-        dest.writeString(this.fileName);
-        dest.writeString(this.title);
-        dest.writeString(this.content);
-        dest.writeString(this.time);
-    }
-
-    @SuppressWarnings("rawtypes")
-    public static final Parcelable.Creator CREATOR = new Parcelable.Creator(){
-        @Override
-        public Album createFromParcel(Parcel source) {
-            return new Album();
-        }
-
-        @Override
-        public Album[] newArray(int size) {
-            return new Album[size];
-        }
-    };
-*/
-    @Override
     public int compareTo(Album o) {
         return o.num.compareTo(this.num);
-    }
+    }*/
 
-    public String getNum(){
+    public int getNum(){
         return num;
     }
 
